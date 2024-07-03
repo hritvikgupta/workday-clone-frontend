490645b1bc831d0c1c3bb6e94fee5ce325c0e1e4
Hritvik Gupta <hritvikgupta@hritviks-laptop.mynetworksettings.com>
2024-07-03 11:29:05 -0500

    updated signup page

--- src/components/signUpPage.jsx
+++ src/components/signUpPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Header from './Header';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const inputStyles = {
   Input: {
